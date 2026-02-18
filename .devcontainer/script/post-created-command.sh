1f2b29a5374da84184e0d4b55a7de88c13ec7f9c
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-02-18 16:54:18 +0000

    🔧 devcontainer のシェル変数クォーティングを統一する

--- .devcontainer/script/post-created-command.sh
+++ .devcontainer/script/post-created-command.sh
@@ -4,7 +4,7 @@ LOCAL_HOME=$1
 
 # REMARKS: ホームディレクトリにホストコンピューターと同等のパスで `.claude` を配置するためのシンボリックリンクを作成する。
 sudo mkdir -p "${LOCAL_HOME}"
-sudo ln -sf "$HOME/.claude" "${LOCAL_HOME}/.claude"
+sudo ln -sf "${HOME}/.claude" "${LOCAL_HOME}/.claude"
 
 proto install
 
