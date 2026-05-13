d3f2d51d7a62f6632ffab51ada23bcbd009fb8b5
Kawano Kenichi <kenichi.k.820@gmail.com>
2026-05-13 16:03:09 +0000

    💡 `promise-chain.ts` の ESLint 抑制コメントに理由を追記する
    `sonarjs/reduce-initial-value` の無効化コメントに初期値が不要な理由を明記し、レビュー時の疑念を解消する。

--- apps/notion-toggl-bridge/src/utils/promise-chain.ts
+++ apps/notion-toggl-bridge/src/utils/promise-chain.ts
@@ -1,3 +1,3 @@
 export const promiseChain = <T>(funcs: (() => Promise<T>)[]) =>
-  // eslint-disable-next-line sonarjs/reduce-initial-value
+  // eslint-disable-next-line sonarjs/reduce-initial-value -- 最初の要素から開始するため、初期値は不要
   funcs.reduce((prev, func) => () => prev().then(() => func()));
